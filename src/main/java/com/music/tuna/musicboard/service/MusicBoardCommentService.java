30ad723cf2b149e170086f4f7b020ebda2077424
Master <seokhyun0324@gmail.com>
2019-10-08 14:50:11 +0900

    댓글 구현 중

--- src/main/java/com/music/tuna/musicboard/service/MusicBoardCommentService.java
+++ src/main/java/com/music/tuna/musicboard/service/MusicBoardCommentService.java
@@ -1,4 +1,10 @@
 package com.music.tuna.musicboard.service;
 
+import com.music.tuna.musicboard.vo.MusicBoardComment;
+
+import java.util.List;
+
 public interface MusicBoardCommentService {
+    public void insertComment(MusicBoardComment vo);
+    public List<MusicBoardComment> getCommentList(MusicBoardComment vo);
 }
