806d3286f94cb7977113f415ad5c1520340eba1c
Master <seokhyun0324@gmail.com>
2019-10-08 15:04:47 +0900

    댓글 삽입 기능 구현

--- src/main/java/com/music/tuna/musicboard/service/MusicBoardCommentServiceImpl.java
+++ src/main/java/com/music/tuna/musicboard/service/MusicBoardCommentServiceImpl.java
@@ -5,6 +5,8 @@ import com.music.tuna.musicboard.vo.MusicBoardComment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MusicBoardCommentServiceImpl implements MusicBoardCommentService {
     @Autowired
@@ -12,5 +14,9 @@ public class MusicBoardCommentServiceImpl implements MusicBoardCommentService {
     @Override
     public void insertComment(MusicBoardComment vo) {
         musicBoardCommentDAO.insertComment(vo);
+    }
+    @Override
+    public List<MusicBoardComment> getCommentList(MusicBoardComment vo) {
+        return null;
     }
 }
