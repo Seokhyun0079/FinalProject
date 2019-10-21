36e1181715053215574223cfb6ba989a65826900
Inseok <chillchillseok@gmail.com>
2019-10-21 11:12:23 +0900

    2019. 10. 12. / 차트 수정

--- src/main/java/com/music/tuna/chart/controller/ChartController.java
+++ src/main/java/com/music/tuna/chart/controller/ChartController.java
@@ -1,10 +1,7 @@
 package com.music.tuna.chart.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.music.tuna.chart.service.ChartService;
 
 @Controller
 @RequestMapping("/chart")
