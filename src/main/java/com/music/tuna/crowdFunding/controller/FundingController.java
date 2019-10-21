f939fb40810425597c79f363836aa6533a154b3a
zoetrope56 <chldpwl5620@gmail.com>
2019-10-21 21:29:44 +0900

    Add multi-file images uploading applied with naver smart-editor

--- src/main/java/com/music/tuna/crowdFunding/controller/FundingController.java
+++ src/main/java/com/music/tuna/crowdFunding/controller/FundingController.java
@@ -41,4 +41,53 @@ public class FundingController {
         return "crowdFunding/fundingEnroll";
     }
 
+    @RequestMapping(value = "/insertFContent.do", method = RequestMethod.POST)
+    public String insertFContent(String editor) {
+        System.out.println("[nse] 저장할 내용 : "+editor);
+        return "redirect:/crowdFunding/fundingEnroll";
+    }
+
+    // 다중 파일 업로드
+    @RequestMapping(value = "/file_uploader_html5.do", method = RequestMethod.POST)
+    @ResponseBody
+    public String multiplePhotoUpload(HttpServletRequest request) {
+        // 파일 정보
+        StringBuffer sb = new StringBuffer();
+        try {
+            // 파일명 받기
+            String oldName = request.getHeader("file-name");
+
+            // 파일 기본경로_상세경로
+            String filePath = "C:\\FinalProject\\src\\main\\webapp\\resources\\editor\\photoUpload\\";
+            String saveName = sb.append(new SimpleDateFormat("yyyyMMddHHmmss")
+                                .format(System.currentTimeMillis()))
+                                .append(UUID.randomUUID().toString())
+                    .append(oldName.substring(oldName.lastIndexOf("."))).toString();
+
+            InputStream is = request.getInputStream();
+            OutputStream os = new FileOutputStream(filePath+saveName);
+
+            int numRead;
+            byte b[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
+
+            while((numRead = is.read(b, 0, b.length))!= -1) {
+                os.write(b, 0, numRead);
+            }
+            os.flush();
+            os.close();
+
+            sb = new StringBuffer();
+            sb.append("&bNewLine=true")
+                    .append("&sFileName=").append(oldName)
+                    .append("&sFileURL=").append("http://localhost:8989/TunaMusic/resources/editor/photoUpload/")
+                    .append(saveName);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("[fundingController] : "+sb.toString());
+        return sb.toString();
+    }
+
 }
