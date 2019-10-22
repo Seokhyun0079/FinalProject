facab4fe158510259998dc55f2f5af84181398b6
Inseok <chillchillseok@gmail.com>
2019-10-22 10:49:34 +0900

    2019. 10. 22 10:49 / 차트 출력 수정

--- src/main/java/com/music/tuna/chart/controller/ChartController.java
+++ src/main/java/com/music/tuna/chart/controller/ChartController.java
@@ -1,18 +1,31 @@
 package com.music.tuna.chart.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.music.tuna.chart.service.ChartService;
+import com.music.tuna.chart.vo.Chart;
 
 @Controller
 @RequestMapping("/chart")
 public class ChartController {
 	
-//	@Autowired
-//	private ChartService chartService;
+	@Autowired
+	private ChartService chartService;
+	
+//	@RequestMapping(value="/chartView.do")
+//	public String chartMain() {
+//		return "chart/chart"; // chart.jsp 리턴
+//	}
 	
 	@RequestMapping(value="/chartView.do")
-	public String chartMain() {
-		return "chart/chart"; // chart.jsp 리턴
+	public ModelAndView getRealTime(ModelAndView mv) {
+		mv.setViewName("chart/chart");
+		mv.addObject("articleList", chartService.selectRealTime());
+		return mv;
 	}
-
+	
 }
